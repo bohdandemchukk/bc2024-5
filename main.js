80a4a0cc94c2aae6dfab6dcc8845ad6facece393
bohdandemchukk bc2024-5 <bogdanchuk2006@gmail.com>
2024-11-03 20:20:49 +0200

    get працює

--- main.js
+++ main.js
@@ -11,7 +11,17 @@ program.parse(process.argv);
 
 const options = program.opts();
 const lab5 = express();
+lab5.use(express.json());
 
+lab5.get('/notes/:note_name', (req, res) => {
+  const path_to_note = path.join(options.cache, `${req.params.note_name}.txt`);
+  fs.readFile(path_to_note, null, (err, data) => {
+    if (err) {
+      return res.status(404).send('Not found');
+    }
+    res.send(data);
+  });
+});
 
 lab5.listen(options.port, options.host, () => {
   console.log(`Server running at http://${options.host}:${options.port}`);
